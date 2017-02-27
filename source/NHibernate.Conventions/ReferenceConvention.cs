f03e69f85bba7d60adf36da7a5fe4b3c8b5e04f2
Dmitry <dmtKlenye@gmail.com>
2017-02-27 17:11:13 +0300

    Добавлены соглашения по автомаппингу NHibernate

---
+++ source/NHibernate.Conventions/ReferenceConvention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using JetBrains.Annotations;
+
+namespace Naftan.Infrastructure.NHibernate.Conventions
+{
+    public class ReferenceConvention : IReferenceConvention
+    {
+        public void Apply(IManyToOneInstance instance)
+        {
+
+            if (Attribute.GetCustomAttributes(instance.Property.MemberInfo, false)
+                .OfType<NotNullAttribute>()
+                .Any()
+                )
+            {
+                instance.Not.Nullable();
+            }
+        }
+    }
+}
